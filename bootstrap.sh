e331cd5665b20ffc9e5a5fbfdfa1b9eb21a0e262
zcodes <zcodes@163.com>
2016-10-18 00:55:49 +0800

    update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,7 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
-if [ ! -d bundle/Vundle.vim ]; then
-    git clone https://github.com/VundleVim/Vundle.vim.git bundle/Vundle.vim
+if [ ! -d ./bundle/Vundle.vim ]; then
+    git clone https://github.com/VundleVim/Vundle.vim.git ./bundle/Vundle.vim
 fi
+
+vim -E -c PluginInstall -c qa
