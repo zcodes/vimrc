abc1ea15e81413a3b8739ec2871a8a3e554687e8
zcodes <zcodes@qq.com>
2018-12-07 10:10:11 +0800

    make bootstrap faster

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@
 
 # install plugins
 if [ ! -d ./bundle/Vundle.vim ]; then
-    git clone https://github.com/VundleVim/Vundle.vim.git ./bundle/Vundle.vim
+    git clone --depth=1 https://github.com/VundleVim/Vundle.vim.git ./bundle/Vundle.vim
 fi
 
 vim -E -c PluginInstall -c qa
