dffe891eb5f37a1c489496a5cd7947fbb4d7eb1b
zcodes <zcodes@qq.com>
2020-12-24 09:58:45 +0800

    update

--- python/format_json.py
+++ python/format_json.py
@@ -10,17 +10,12 @@ import io
 
 def main():
     prog = 'python -m json.tool'
-    description = ('A simple command line interface for json module '
-                   'to validate and pretty-print JSON objects.')
+    description = ('A simple command line interface for json module ' 'to validate and pretty-print JSON objects.')
     parser = argparse.ArgumentParser(prog=prog, description=description)
-    parser.add_argument('infile', nargs='?', type=argparse.FileType(),
-                        help='a JSON file to be validated or pretty-printed')
-    parser.add_argument('outfile', nargs='?', type=argparse.FileType('w'),
-                        help='write the output of infile to outfile')
-    parser.add_argument('--sort-keys', action='store_true', default=False,
-                        help='sort the output of dictionaries alphabetically by key')
-    parser.add_argument('--decode-unicode', action='store_true', default=False,
-                        help='decode the encoded unicode data')
+    parser.add_argument('infile', nargs='?', type=argparse.FileType(), help='a JSON file to be validated or pretty-printed')
+    parser.add_argument('outfile', nargs='?', type=argparse.FileType('w'), help='write the output of infile to outfile')
+    parser.add_argument('--sort-keys', action='store_true', default=False, help='sort the output of dictionaries alphabetically by key')
+    parser.add_argument('--decode-unicode', action='store_true', default=False, help='decode the encoded unicode data')
     options = parser.parse_args()
 
     infile = options.infile or io.TextIOWrapper(sys.stdin.buffer, encoding='utf-8')
@@ -32,8 +27,7 @@ def main():
             if sort_keys:
                 obj = json.load(infile)
             else:
-                obj = json.load(infile,
-                                object_pairs_hook=collections.OrderedDict)
+                obj = json.load(infile, object_pairs_hook=collections.OrderedDict)
         except ValueError as e:
             raise SystemExit(e)
     with outfile:
