2974d0b6ac1f0499661fbfae4c1e201f2f5ed2bb
zcodes <zcodes@qq.com>
2021-02-01 15:37:27 +0800

    update plug.vim

--- update-vim-plug.sh
+++ update-vim-plug.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env sh
 
+# curl -fLo autoload/plug.vim --create-dirs \
+#     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 curl -fLo autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    https://raw.fastgit.org/junegunn/vim-plug/master/plug.vim
